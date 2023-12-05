17570a9c6231030d3c617f98f2521cdf090f13b4
MarcoCastellani <marcocastellanictt@gmail.com>
2023-12-05 14:30:34 -0300

    ClassPosition

---
+++ Java02/EstudosBasicos/src/Aplication/Main.java
@@ -0,0 +1,15 @@
+package Aplication;
+
+import BoardGame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position peao1 = new Position(1,2);
+        System.out.println(peao1.toString());
+
+
+
+        
+    }
+}
